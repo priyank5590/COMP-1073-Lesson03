74283eb39e480f13598926f54bddaa99b32640dc
priyank5590 <vyaspriyank5@gmail.com>
2016-01-28 07:51:06 -0800

    first commit

---
+++ script/app.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+"use strict";
+
+var username = "Jimmy";
+var age = 30;
+var ishungry = true;
+var usernames = [];
+
+usernames = ["mike", "rich", "jimmy", "naruto"];
+console.log("before the push: "  +  usernames);
